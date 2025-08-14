cb37de3ac24873a3763fbaebb9164fa45b9a0fa0
unknown <b12202069@ntu.edu.tw>
2025-08-14 18:20:12 +0800

    chore: ignore node_modules/dist/.astro and save local changes

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://weihsuanchung.github.io',
-  base: '/NASA-astro/', 
-  integrations: [tailwind()],
-});
